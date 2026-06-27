78ee2b1c5cf98045c51c83a5dc8124aa40b56d5f
Victor <atendejasolucoesautomaticas@gmail.com>
2026-06-27 07:57:36 -0300

    general

---
+++ CUBANOS_BR_MARCOS/DASHBOARD Financiero /config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: 'https://rcqkmaxkuxllcyjzqbvn.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjcWttYXhrdXhsbGN5anpxYnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzU3NzQsImV4cCI6MjA5NjIxMTc3NH0.3JlpvnK5OqjMWGAP3jl97VXknCi98Bks70owBe1jOz8',
+  ano: 2026,
+};
